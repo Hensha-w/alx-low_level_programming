fe3eb4ec5635102d9c783696e8f7ae6042e69176
Hensha-w <himtwo024@gmail.com>
2024-01-15 15:52:00 -0800

    done

--- 0x18-dynamic_libraries/0-isupper.c
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -1,17 +1,17 @@
 #include "main.h"
-#include <stdio.h>
 /**
- * _isupper - check if c is upper
- *
- * @c: input for character
- *
- * Return: 1 if its uppercase, 0 if not
-*/
-
+ * _isupper - checks for uppercase character
+ * @c: Variable text
+ * Return: Always 0.
+ */
 int _isupper(int c)
 {
-	if (c >= 65 && c <= 90)
+	if (c >= 'A' && c <= 'Z')
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
+	}
 }
--- 0x18-dynamic_libraries/3-islower.c
+++ 0x18-dynamic_libraries/3-islower.c
@@ -1,16 +1,15 @@
 #include "main.h"
-
 /**
- *_islower - checks for a lowercase character
- *@c: letter being tested
- * Return: Always 0
+ * _islower - Checks for lowercase character
+ * @c: The character to be checked
+ * Return: 1 for lowercase character or 0 for anything else
  */
 
 int _islower(int c)
 {
-	if  (c >= 97 && c <= 122)
+	if (c >= 97 && c <= 122)
+	{
 		return (1);
-	else
-		return (0);
-
+	}
+	return (0);
 }
